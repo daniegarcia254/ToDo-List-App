625ba9e730518f3fe76a8b46195ada233849d91d
daniegarcia254 <daniegarcia254@gmail.com>
2015-02-08 02:33:04 +0100

    Changed behavior of the angular service "ToDosService". Now it's possible to add more functions.

--- ToDo_App_Nodejs/public/javascripts/angularApp.js
+++ ToDo_App_Nodejs/public/javascripts/angularApp.js
@@ -1,19 +1,25 @@
 (function() {
   var app = angular.module('toDoApp', []);
 
-  var API_URI = "http://localhost:8080/todolist";	//REST endpoint URI
+  var API_URI = "http://localhost:3000/todos";	//REST endpoint URI
   
   /*-----------------------------------
 	SERVICES
   -------------------------------------*/
   //Service to retrieve the complete ToDo list of the repository
-  app.factory('toDoListService', function($http) {
+  app.factory('ToDosService', function($http) {
 
-	  return {
-		getToDoList: function(cb) {
-			$http.get(API_URI).success(cb);
-		}
-	   }
+	  var t = {
+		  todos: []
+	  };
+
+	  t.getAll = function() {
+			return $http.get(API_URI).success(function(data){
+				angular.copy(data, t.todos);
+			});
+	  };
+
+	  return t;
   });
   
   
@@ -26,29 +32,31 @@
   });
   
   //Controller for navigation tabs
-  app.controller('tabCtrl', ['$scope', '$rootScope', 'toDoListService', function($scope, $rootScope, toDoListService){
+  app.controller('tabCtrl', ['$scope', '$rootScope', 'ToDosService', function($scope, $rootScope, ToDosService){
+
+	$rootScope.toDos = ToDosService.todos;
+
 	$scope.getToDoList = function(tabName){
-		//Uses the a service for get the list
-		toDoListService.getToDoList(function(data) {
-			$rootScope.toDos = data;
-		});
+
+		ToDosService.getAll();
+
 		$rootScope.errorMessage = "";
 		if (tabName == 'add') $rootScope.hideValue = "true";
 		else if (tabName == 'list') $rootScope.hideValue = "true";
 		else if (tabName = 'remove') $rootScope.hideValue = "false";
 	};
   }]);
-  
+
   //Controller for the "Add ToDo" form
-  app.controller('addToDoFormCtrl', ['$scope', '$rootScope', '$http', 'toDoListService', function($scope, $rootScope, $http, toDoListService){
+  app.controller('addToDoFormCtrl', ['$scope', '$rootScope', '$http', 'ToDosService', function($scope, $rootScope, $http, ToDosService){
 	$scope.addToDo = function() {
 		var todo=$scope.fields;	//Take the form fields
 		$scope.fields = "";  //Clean the form
-		
+
 		//Make a POST call to add the ToDo to the repository
 		var res = $http.post(API_URI, todo);
 		res.success(function(data, status, headers, config) {
-			toDoListService.getToDoList(function(data) {
+			ToDosService.getAll(function(data) {
 				$rootScope.toDos = data;
 			});
 		});
@@ -57,9 +65,9 @@
 		});
 	};
   }]);
-  
+
   //Controller for the "List ToDo's" form
-   app.controller('listToDosFormCtrl', ['$scope', '$rootScope', '$http', 'toDoListService', function($scope, $rootScope, $http, toDoListService){
+   app.controller('listToDosFormCtrl', ['$scope', '$rootScope', '$http', 'ToDosService', function($scope, $rootScope, $http, ToDosService){
     $scope.selectors = fields;
 	$scope.listToDos = function() {
 		var query_bis = $scope.query;
@@ -78,12 +86,12 @@
 		console.log($scope.selector);
 	}
   }]);
-  
+
   //Controller for the "List All ToDo's" form
-   app.controller('listAllFormCtrl', ['$scope', '$rootScope', '$http', 'toDoListService', function($scope, $rootScope, $http, toDoListService){
+   app.controller('listAllFormCtrl', ['$scope', '$rootScope', '$http', 'ToDosService', function($scope, $rootScope, $http, ToDosService){
 	$scope.listAll = function() {
 		//Uses the a service for get the list
-		toDoListService.getToDoList(function(data) {
+		ToDosService.getAll(function(data) {
 			$rootScope.toDos = data;
 			if (data.toDoList.length === 0){
 				$rootScope.errorMessage = "The repository is empty!";
@@ -93,9 +101,9 @@
 		});
 	};
   }]);
-  
+
   //Controller for the "Remove ToDo's" form
-   app.controller('removeToDosFormCtrl', ['$scope', '$rootScope', '$http', 'toDoListService', function($scope, $rootScope, $http, toDoListService){
+   app.controller('removeToDosFormCtrl', ['$scope', '$rootScope', '$http', 'ToDosService', function($scope, $rootScope, $http, ToDosService){
     $scope.selectors = fields;
 	$scope.removeToDos = function() {
 		var query_bis = $scope.query;
@@ -111,9 +119,9 @@
 		$scope.query = "";
 	};
   }]);
-  
+
   //Controller for the "Remove All ToDo's" form
-   app.controller('removeAllFormCtrl', ['$scope', '$rootScope', '$http', 'toDoListService', function($scope, $rootScope, $http, toDoListService){
+   app.controller('removeAllFormCtrl', ['$scope', '$rootScope', '$http', 'ToDosService', function($scope, $rootScope, $http, ToDosService){
 	$scope.removeAll = function() {
 		$http.delete(API_URI+'/removeAll')
 			.success(function(data, status, headers, config) {
@@ -128,8 +136,8 @@
   }]);
 
   //Controller for keeping updated the table with the ToDo's
-  app.controller('showResultCtrl', ['$scope', '$rootScope', '$http', 'toDoListService', function($scope, $rootScope, $http, toDoListService){
-	toDoListService.getToDoList(function(data) {
+  app.controller('showResultCtrl', ['$scope', '$rootScope', '$http', 'ToDosService', function($scope, $rootScope, $http, ToDosService){
+	ToDosService.getAll(function(data) {
 		$rootScope.toDos = data;
 	});
 	
@@ -167,7 +175,7 @@
   -------------------------------------*/
   var actions = [{
 		title: 'Add ToDo',
-		description: "This will allow to you add a ToDo task to the repository.",
+		description: "This will allow to you add a ToDo task to the repository."
     }, {
 		title: 'List ToDo\'s',
 		description: "This will allow you to list all the repository ToDo's or make a filtered search of ToDo's by any of its fields."
