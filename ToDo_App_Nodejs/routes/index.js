815cc529c523081490d4fde3a87940408b36c285
daniegarcia254 <daniegarcia254@gmail.com>
2015-02-08 01:32:38 +0100

    Created a GET route for retrieving all the ToDos

--- ToDo_App_Nodejs/routes/index.js
+++ ToDo_App_Nodejs/routes/index.js
@@ -1,7 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var mongoose = require('mongoose');
+var ToDo = mongoose.model('ToDo');
+
+/* GET all ToDos. */
+router.get('/todos', function(req, res, next) {
+  ToDo.find(function(err, posts){
+    if(err){ return next(err); }
+
+    res.json(posts);
+  });
+});
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
