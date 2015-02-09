085e47321777f95d910f8a0febe2374547782a29
daniegarcia254 <daniegarcia254@gmail.com>
2015-02-09 08:24:03 +0100

    Rename of vars

--- ToDo_App_Nodejs/routes/index.js
+++ ToDo_App_Nodejs/routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var todo = mongoose.model('ToDo');
+var ToDo = mongoose.model('ToDo');
 
 /* GET all ToDos. */
 router.get('/todos', function(req, res, next) {
-  todo.find(function(err, todos){
+  ToDo.find(function(err, todos){
     if(err){ return next(err); }
 
     res.json(todos);
@@ -28,23 +28,23 @@ router.get('/todos/:tag/:query', function(req, res, next) {
 
   switch (req.params.tag) {
     case 'description':
-      todo.find({task: q}, cb);
+      ToDo.find({task: q}, cb);
       break;
     case 'context':
-      todo.find({context: q}, cb);
+      ToDo.find({context: q}, cb);
       break;
     case 'project':
-      todo.find({project: q}, cb);
+      ToDo.find({project: q}, cb);
       break;
     case 'priority':
-      todo.find({priority: q}, cb);
+      ToDo.find({priority: q}, cb);
   }
 });
 
 /* POST --> create a new ToDo */
 router.post('/todos', function(req, res, next) {
 
-  var todo = new todo(req.body);
+  var todo = new ToDo(req.body);
 
   todo.save(function(err, todo){
     if(err){ return next(err); }
