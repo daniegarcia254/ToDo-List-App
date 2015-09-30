2b0ff46e990c6ad471ee83f93180bd797289e72f
daniegarcia254 <daniegarcia254@gmail.com>
2015-09-30 22:49:21 +0200

    Behaviour of the Remove ToDo's Form. Hide and show the correspondent input depending on the "Search by" selected option.

--- ToDo_App_Nodejs/public/javascripts/angularApp.js
+++ ToDo_App_Nodejs/public/javascripts/angularApp.js
@@ -150,19 +150,26 @@
 	});
 
   	//Controller for the "Remove ToDo's" form
-  	app.controller('removeToDosFormCtrl', function($scope, $rootScope, $http, ToDosService, $timeout){
+  	app.controller('removeToDosFormCtrl', function($scope, $rootScope, $http, ToDosService, $timeout, $compile){
 
 		$scope.selectors = fields;
+		$scope.taskField = false;
+		$scope.dateField = false;
+		$scope.statusField = false;
+		$scope.priorityField = false;
 
 		//Show & hide the form
 		$rootScope.removeTodoFormShow = false;
 		$scope.closeRemoveToDoForm = function(){
-		  $rootScope.removeTodoFormShow = !$rootScope.removeTodoFormShow;
+			$rootScope.removeTodoFormShow = !$rootScope.removeTodoFormShow;
+			$scope.selector = undefined;
+			$scope.query = "";
 		};
 
-	  	//Initiate the form remove input as text type
+	  	//Initiate the form remove input with Task Description input
 	  	if (typeof $scope.selector === 'undefined') {
-		  	$rootScope.hideInputRemovePriority = $rootScope.showInputRemove = true;
+		  	$rootScope.hideRemoveFormDateInput = $rootScope.hideRemoveFormStatusInput =
+				$rootScope.hideRemoveFormPriorityInput = $rootScope.showRemoveFormTaskInput = true;
 	  	}
 
 	  	//Function for remove in the DB the ToDo's that match the user input query
@@ -171,9 +178,6 @@
 		  ToDosService.removeToDos($scope.selector.tag, $scope.query, function() {
 			  //Get the complete list of ToDo's after removing
 			  ToDosService.getAll(function(data){
-				  //Clear formulary and hide formulary
-				  $scope.query = "";
-				  $scope.selector =  undefined;
 				  $scope.closeRemoveToDoForm();
 				  $timeout(function(){
 					  $scope.checkRemoveInputType();
@@ -188,31 +192,56 @@
 
 	  	$rootScope.submitRemoveDisabled = true;
 	  	//Function that handles if the submit button is enabled
-	  	$scope.checkEnableRemoveFormSubmitButton = function(element){
+	  	$scope.checkEnableRemoveFormSubmitButton = function(){
 		  if (typeof $scope.selector == 'undefined') {
 			  $rootScope.submitRemoveDisabled = true;
 		  } else {
-			  $rootScope.submitRemoveDisabled =  !(typeof $scope.query != 'undefined');
+			  $rootScope.submitRemoveDisabled = !(typeof $scope.query != 'undefined' && $scope.query.length > 0);
 		  }
 	  	};
 
 	  	//Function that handles the formulary input element change of type
 	  	$scope.checkRemoveInputType = function() {
-		  if (typeof $scope.selector === 'undefined') {
-			  $rootScope.hideInputRemovePriority = $rootScope.showInputRemove = true;
-		  } else {
-			  if ($scope.selector.tag != 'priority') {
-				  $rootScope.hideInputRemovePriority = true;
-				  $timeout(function () {
-					  $rootScope.showInputRemove = true;
-				  }, 500);
-			  } else {
-				  $rootScope.showInputRemove = false;
-				  $timeout(function () {
-					  $rootScope.hideInputRemovePriority = false;
-				  }, 500);
-			  }
-		  };
+			var $parent = $('#removeToDoFormField');
+			var $div = null;
+			if (typeof $scope.selector == 'undefined') {
+			} else {
+
+				if ($scope.taskField || $scope.dateField || $scope.statusField || $scope.priorityField) {
+					$parent.find('> .ng-scope').remove();
+					$scope.query = '';
+					$scope.checkEnableRemoveFormSubmitButton();
+				}
+
+				$scope.taskField = $scope.dateField = $scope.statusField = $scope.priorityField = false;
+
+				switch ($scope.selector.tag) {
+					case 'description':
+						$div = $('<todo-remove-form-task-field></todo-remove-form-task-field>');
+						$scope.taskField = true;
+						break;
+					case 'date':
+						$div = $('<todo-remove-form-date-field></todo-remove-form-date-field>');
+						$scope.dateField = true;
+						break;
+					case 'status':
+						$div = $('<todo-remove-form-status-field></todo-remove-form-status-field>');
+						$scope.statusField = true;
+						break;
+					case 'priority':
+						$div = $('<todo-remove-form-priority-field></todo-remove-form-priority-field>');
+						$scope.priorityField = true;
+						break;
+				}
+				setTimeout(function () {
+					angular.element($parent).injector().invoke(function ($compile) {
+						var $parentScope = angular.element($parent).scope();
+						$parent.append($compile($div)($parentScope));
+						$parentScope.$apply();
+					});
+				}, 100);
+				//$('#removeToDoForm').find('#formButton').prop("disabled", true);
+			}
 	  	};
   });
 
